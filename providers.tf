872e04c63abdc954c4309836944a62bcbff84e85
kalyaneswar <kalyaneswarreddy16696@gmail.com>
2026-03-14 12:45:00 +0530

    terraform

--- providers.tf
+++ providers.tf
@@ -6,8 +6,15 @@ terraform {
       version = "~> 6.0"
     }
   }
+  backend "s3" {
+    bucket = "kalyan-remote-state"
+    key    = "sg-test"
+    region = "us-east-1"
+    dynamodb_table = "kalyaneswar-locking"
+  }
 }
 
 # provider "aws" {
 #   region = "us-east-1"
 # }
+
