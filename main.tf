0f5c4ce1e5627002936c86030f3ded303025e552
kalyaneswar <kalyaneswarreddy16696@gmail.com>
2026-03-14 12:07:47 +0530

    terraform

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 
 # Security Group
 resource "aws_security_group" "new_sg" {
-  name        = "allow_tls"
+  name        = "new_sg"
   description = "Allow TLS inbound traffic and all outbound traffic"
   vpc_id      = "vpc-01fa20f567ec3d498"
 
   tags = {
-    Name = "allow_tls"
+    Name = "new_sg"
   }
 }
