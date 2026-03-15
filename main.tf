b2cfd0ccf0944513738ac28f6c85a5feb21001d0
kalyaneswar <kalyaneswarreddy16696@gmail.com>
2026-03-15 16:21:31 +0530

    terraform

--- main.tf
+++ main.tf
@@ -5,7 +5,32 @@ resource "aws_security_group" "new_sg" {
   description = "Allow TLS inbound traffic and all outbound traffic"
   vpc_id      = "vpc-01fa20f567ec3d498"
 
+   egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1" #all
+    cidr_blocks = var.allowed_cidr
+  }
+
+   ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    cidr_blocks = var.allowed_cidr
+  }
+
+  tags = var.tags
+}
+
+resource "aws_instance" "expense" {
+  ami           =var.ami_id
+  instance_type = var.instance_name == "db" ? "t3.micro" : "t2.micro"
+  vpc_security_group_ids = [ aws_security_group.new_sg.id ]
+#   count = 3
+  count = length(var.instance_name)
+
   tags = {
-    Name = "new_sg"
+    Name = var.instance_name[count.index]
+    
   }
 }
---
+++ variables.tf
@@ -0,0 +1,36 @@
+variable "tags" {
+  default = {
+    Project = "My Own Project"
+    Environment = "Prod"
+    Module = "Frontend" 
+    Project_id = 123456
+  }
+}
+
+variable "allowed_cidr" {
+  type = list(string)
+  default = [ "0.0.0.0/0" ]
+}
+
+variable "ami_id" {
+  type = string
+  default = "ami-0220d79f3f480ecf5"
+}
+
+variable "instance_name" {
+  type = list(string)
+  default = ["frontend", "backend", "db"]
+}
+
+
+##################################################
+################   Route 53  #####################
+##################################################
+
+variable "zone_id" {
+  default = "Z03061823K58PEFROH5QA"
+}
+
+variable "domain_name" {
+  default = "kalyaneswar.site"
+}
